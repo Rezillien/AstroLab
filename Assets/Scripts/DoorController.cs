a79a52b87dfb3d1c88cd42440bdfc9cd978859b4
Rezilien <kaziorradoslaw@gmail.com>
2016-11-19 20:04:23 +0100

    first branch

---
+++ Assets/Scripts/DoorController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void use()
+    {
+        GetComponent<BoxCollider2D>().enabled = false;
+        GetComponent<SpriteRenderer>().sprite = "spritesAll_2";
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Use this for initialization
+    StartBoardManager board;
+    int x;
+    int y;
+    int framesToNextMove;
+    int speed = 15;
+
+    void Start()
+    {
+        x = 1;
+        y = 1;
+        framesToNextMove = speed;
+        Setup();
+    }
+
+    public void Setup()
+    {
+        board = GameManager.instance.getBoard();
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        --framesToNextMove;
+
+        if (framesToNextMove > 0)
+        {
+            return;
+        }
+
+        int dx = 0;
+        int dy = 0;
+        float ha = Input.GetAxis("Horizontal");
+        float va = Input.GetAxis("Vertical");
+        if (ha > 0.0f) dx = 1;
+        else if (ha < 0.0f) dx = -1;
+        else if (va > 0.0f) dy = 1;
+        else if (va < 0.0f) dy = -1;
+        if (board.canMove(x + dx, y + dy))
+        {
+            x += dx;
+            y += dy;
+        }
+
+        transform.position = new Vector3(x, y, 0);
+
+        if(dx != 0 || dy != 0)
+        {
+            framesToNextMove = speed;
+        }
+    }
+}
