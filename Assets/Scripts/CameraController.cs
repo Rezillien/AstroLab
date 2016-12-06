bb64b4eb839801fc4ece8e518cf50bc78f67ae6a
Rezilien <kaziorradoslaw@gmail.com>
2016-12-06 16:49:27 +0100

    add right mouse key camera controll

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour 
+{
+
+	private Vector3 mousePositionFirst;
+	private Vector3 mousePositionSecond;
+	private Vector3 transformVector;
+	public float moveSpeed=1f;
+
+	private bool firstFrame=true;
+
+	// Use this for initialization
+	void Start () 
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetMouseButton (1)) {
+			if (firstFrame) {
+				firstFrame = false;
+				mousePositionSecond = Input.mousePosition;
+				mousePositionSecond = Camera.main.ScreenToWorldPoint (mousePositionSecond);
+			} else {
+				mousePositionFirst = mousePositionSecond;
+				mousePositionSecond = Input.mousePosition;
+				mousePositionSecond = Camera.main.ScreenToWorldPoint (mousePositionSecond);
+				transformVector = new Vector3 (mousePositionFirst.x - mousePositionSecond.x, mousePositionFirst.y - mousePositionSecond.y, -10f);
+				transform.position = new Vector3 (transform.position.x + transformVector.x * moveSpeed, transform.position.y + transformVector.y * moveSpeed, transformVector.z);
+			}
+		} else
+			firstFrame = true;
+
+	}
+}
+
